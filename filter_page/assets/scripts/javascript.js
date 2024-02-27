4690b4fbf90774092821019c29c873aeaa475922
Denowsky <den.san404@gmail.com>
2024-02-27 21:40:49 +0300

    Реализован парс данных. Добавлены фильтры. Бренд сделан в виде выпадающего списка

--- filter_page/assets/scripts/javascript.js
+++ filter_page/assets/scripts/javascript.js
@@ -1,9 +1,14 @@
-const stamp = new Date().toISOString().slice(0, 10).replace(/-/g, "");
+async function init() {
+    await get_data(get_ids, ids);
+    await get_data(get_brands, brands);
+    await get_data(get_items, items);
+    await append_options(brands)
+    console.log(ids);
+    console.log(brands);
+    console.log(items);
+}
 
-const hash = md5(`${'Valantis'}_${stamp}`);
-const url = 'http://api.valantis.store:40000/';
-
-function get_data(action, data_list) {
+async function get_data(action, data_list) {
     const request = new Request(url, {
         method: "POST",
         body: JSON.stringify(action),
@@ -13,20 +18,39 @@ function get_data(action, data_list) {
         }
     });
 
-    fetch(request)
-        .then(response => response.json())
-        .then(action => {
-            var data = action.result;
-            for (var id in data) {
-                if (data[id] != null) {
-                    data_list.push(data[id])
-                }
+    try {
+        const response = await fetch(request);
+        const action = await response.json();
+        const data = action.result;
+
+        for (const id in data) {
+            if (data[id] != null) {
+                data_list.push(data[id]);
             }
-        })
-        .catch(error => console.error(error));
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-const get_ids = {
+async function append_options(options) {
+    const brandFilter = document.getElementById('brand-filter');
+    options.forEach(brand => {
+        const option = document.createElement('option');
+        option.value = brand;
+        option.textContent = brand;
+        brandFilter.appendChild(option);
+        console.log(brand);
+    });
+}
+
+const stamp = new Date().toISOString().slice(0, 10).replace(/-/g, "");
+
+const hash = md5(`${'Valantis'}_${stamp}`);
+const url = 'http://api.valantis.store:40000/';
+
+var ids = [];
+var get_ids = {
     "action": "get_ids",
     "params": {
         "offset": 1,
@@ -34,31 +58,23 @@ const get_ids = {
     }
 };
 
-var get_items = {
-    "action": "get_items",
-    "params": {
-        "ids": ids
-    }
-};
-
+var brands = [];
 var get_brands = {
     "action": "get_fields",
     "params": {
         "field": "brand",
-        "offset": 3,
-        "limit": 5
+        "offset": 1,
+        "limit": 10
     }
 };
 
-var ids = [];
-get_data(get_ids, ids);
-console.log(ids);
-
-var brands = [];
-get_data(get_brands, brands);
-console.log(brands);
-
 var items = [];
-get_data(get_items, items);
-console.log(items)
+var get_items = {
+    "action": "get_items",
+    "params": { "ids": ids }
+};
 
+let currentPage = 1;
+const productsPerPage = 50;
+
+init()
