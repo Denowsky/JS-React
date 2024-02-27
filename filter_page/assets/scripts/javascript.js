946e9f9881af3809a64dbda011758e4254a36e65
Denowsky <denowsky26@yahoo.com>
2024-02-27 16:46:27 +0300

    Добавлена функция получения списка из API

--- filter_page/assets/scripts/javascript.js
+++ filter_page/assets/scripts/javascript.js
@@ -1,33 +1,51 @@
 const stamp = new Date().toISOString().slice(0, 10).replace(/-/g, "");
 
 const hash = md5(`${'Valantis'}_${stamp}`);
-console.log(hash);
 const url = 'http://api.valantis.store:40000/';
 
-const data = {
-  "action": "get_ids",
-  "params": {
-    "offset": 1,
-    "limit": 3
-  }
+function get_data(action, data_list) {
+    const request = new Request(url, {
+        method: "POST",
+        body: JSON.stringify(action),
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Auth': hash
+        }
+    });
+
+    fetch(request)
+        .then(response => response.json())
+        .then(action => {
+            var data = action.result;
+            for (var id in data) {
+                data_list.push(data[id])
+            }
+        })
+        .catch(error => console.error(error));
+}
+
+const get_ids = {
+    "action": "get_ids",
+    "params": {
+        "offset": 1,
+        "limit": 10
+    }
 };
 
-const request = new Request(url, {
-  method: "POST",
-  body: JSON.stringify(data),
-  headers: {
-    'Content-Type': 'application/json',
-    'X-Auth': hash
-  }
-});
+var ids = [];
+get_data(get_ids, ids);
+console.log(ids)
 
-var product_list;
+var get_items = {
+    "action": "get_items",
+    "params": {
+        "ids": ids
+    }
+};
 
-fetch(request)
-  .then(response => response.json())
-  .then(data => {
-    const result = data.result;
-    product_list = result;
-    console.log(product_list);
-  })
-  .catch(error => console.error(error));
+var items = [];
+if (ids.length > 0) {
+    get_data(get_items, items);
+    console.log(items)
+}
+
